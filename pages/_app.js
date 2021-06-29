60f262d4969d42dd9266d9fefd143f74e02da581
Daniel James <djames@alternativeairlines.com>
2021-06-29 12:54:52 +0100

    add twitter meta data

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,7 @@ export default function MyApp({ Component, pageProps }) {
     return <>
         <Head>
             <link rel="stylesheet" href="https://use.typekit.net/iuc5xfk.css" />
-            <meta property="og:description" content="monolith ⏵ a blog/wiki/knowledge-base/thought-dump by cesque" />
-            <meta name="theme-color" content="#000000" />
+            getMetaData()
         </Head>
 
         <Component {...pageProps} />
